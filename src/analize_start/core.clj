c155bd37bafe6afb078dde1232e5f02ce0ad64b9
aqude <aqude@localhost.localdomain>
2023-02-24 18:51:21 +0300

    mini script

--- src/analize_start/core.clj
+++ src/analize_start/core.clj
@@ -1,7 +1,26 @@
 (ns analize-start.core)
+; Импортируем модуль для удаления файлов
+(use '[clojure.java.io :as io])
 
-(require '[clojure.java.io.file :as file])
+(defn clean-file []
+  (->> "/home/aqude/Desktop/tests/nonFilter.txt"
+       (io/reader)
+       (line-seq)
+       (map #(clojure.string/replace % ",," ","))
+       (map #(clojure.string/replace % ".,", ","))
+       (map #(clojure.string/replace % "!,", ","))
+       (remove #(clojure.string/blank? %))
+       (map #(clojure.string/trim %))
+       (filter #(clojure.string/includes? % ", "))
+       (map #(let [[text num-str] (clojure.string/split % #",")]
+               {:text (clojure.string/trim text)
+                :num (Integer/parseInt (clojure.string/trim num-str))}))
+       (remove #(or (nil? (:num %)) (not (number? (:num %)))))
+       (sort-by :num)
+       (map #(str (:text %) ", " (:num %)))
+       (clojure.string/join "\n")
+       (io/writer "/home/aqude/Desktop/tests/sorted_file.txt")
+       (.close)))
 
-(if (file/exists? "src/file.txt")
-  (println "Файл существует")
-  (println "Файл не существует"))
+;(clean-file)
+;(io/delete-file "/home/aqude/Desktop/tests/sorted_file.txt")
